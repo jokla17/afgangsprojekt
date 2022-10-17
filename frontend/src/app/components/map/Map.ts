1f1bebda16112e7804a3648f7b443b2b563f3250
Themi21 <Atemberg@gmail.com>
2022-10-17 15:53:52 +0200

    Setup simple map
    Simple map created
    interface for map created

---
+++ frontend/src/app/components/map/Map.ts
@@ -0,0 +1,5 @@
+export interface Map {
+  id: number,
+  latitude: number,
+  longitude: number
+}
---
+++ frontend/src/app/components/map/map.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { OSM } from 'ol/source';
+import TileLayer from 'ol/layer/Tile';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  public map!: Map;
+  ngOnInit(): void {
+    this.initMap();
+ }
+
+ initMap(): void{
+  this.map = new Map({
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    target: 'map',
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+      maxZoom: 18,
+    }),
+  });
+ }
+}
