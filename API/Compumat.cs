4f58fa20156d2b302c35bea687b9bc82030d673a
Themi21 <Atemberg@gmail.com>
2022-10-18 16:16:03 +0200

    Made methods to get one or several compumat units
    Made methods to get a single or a list of  compumat units. If a single is chosen, a id is needed.

---
+++ API/Compumat.cs
@@ -0,0 +1,22 @@
+﻿namespace API
+{
+    public class Compumat
+    {
+
+        public string? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public double Longitude { get; set; }
+
+        public double Latitude { get; set; }
+
+        public CompumatType Type { get; set; }
+
+        public enum CompumatType {
+            VENDINGMACHINE = 1,
+            GATE = 2
+        };
+
+    }
+}
---
+++ API/Controllers/CompumatController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+
+namespace API.Controllers
+{
+    [EnableCors("myPolicy")]
+    [ApiController]
+    [Route("[controller]")]
+    public class CompumatController: ControllerBase
+    {
+        private readonly ILogger<MapController> _logger;
+
+        private Compumat[] compumats =  new Compumat[]
+        { 
+            new Compumat{
+            Id = "01",
+            Name = "Automat ved vandland",
+            Latitude = 55.11404492554651,
+            Longitude = 9.787099052273176,
+            Type = Compumat.CompumatType.VENDINGMACHINE
+            },
+            new Compumat{
+            Id = "02",
+            Name = "Gate ved hovedbygning",
+            Latitude = 55.11404492554651,
+            Longitude = 9.787099052273176,
+            Type = Compumat.CompumatType.GATE
+            },
+            new Compumat{
+            Id = "03",
+            Name = "Gate ved vandland",
+            Latitude = 55.11404492554651,
+            Longitude = 9.787099052273176,
+            Type = Compumat.CompumatType.GATE
+            },
+            new Compumat{
+            Id = "04",
+            Name = "Automat ved hovedbygning",
+            Latitude = 55.11404492554651,
+            Longitude = 9.787099052273176,
+            Type = Compumat.CompumatType.VENDINGMACHINE
+            }
+        };
+
+        public CompumatController(ILogger<MapController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}/Compumat")]
+        public Compumat Get(string id)
+        {
+            return this.compumats.First<Compumat>(obj => (String.Compare(obj.Id, id) == 0));
+        }
+
+        [HttpGet("/Compumats")]
+        public Compumat[] GetAll()
+        {
+            return compumats;
+        }
+    }
+}
